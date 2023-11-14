61f7f417bdfd09f40ab8e34664907faeb1ace854
luizantoniofc <150301604+luizantoniofc@users.noreply.github.com>
2023-11-14 23:07:51 +0000

    Tags completa

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,18 @@ import Card from './components/Card/Card';
 function App() {
   const item1 = {
     name: "Rick Sanchez",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags: ["Status: Vivo", "Espécie Humana", "Origem: Terra C-137"]
     }
   const item2 = {
     name: "Morty Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    tags: ["Status: Em Andamento", "Espécie Animal", "Origem: Terra C-137"]
     }
   const item3 = {
     name: "Summer Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    tags: ["Status: Vivo", "Espécie Humana", "Origem: Terra 661"]
     }
   const items = [item1, item2, item3, item1, item2, item3]
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,17 @@
 import Tag from "../Tag/Tag"
 export default function Card(props) {
   const item = props.item
-  
+
+  const tags = item.tags || []
+
+
   console.log(props)
     return <div className="card">
     <h2>{item.name}</h2>
     <div className="tags">
-      <Tag text="Status: Vivo" />
-      <Tag text="Espécie: Humana" />
-      <Tag text="Origem: Terra C-137"/>
+      {tags.map(function (element) {
+        return <Tag text={element}/>
+      })}
     </div>
     <img src={item.imageUrl} />
   </div>
