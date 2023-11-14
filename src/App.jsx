3f3f4aebafd1df7376ebd0323254a675aab2f294
luizantoniofc <150301604+luizantoniofc@users.noreply.github.com>
2023-11-14 00:39:50 +0000

    Criação do Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      
+      <br />
+      <Card />
       
     </>
   )
