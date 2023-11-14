e31a215eda589fa429630e20993bb35367624a18
luizantoniofc <150301604+luizantoniofc@users.noreply.github.com>
2023-11-14 01:27:59 +0000

    Criação da Lista

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,20 @@ function App() {
     imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
     }
   const item2 = {
-    name: "Morty",
+    name: "Morty Smith",
     imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
     }
+  const item3 = {
+    name: "Summer Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    }
+  const items = [item1, item2, item3, item1, item2, item3]
 
   return (
     <>
-      
-      <br />
-      <Card item={item1}/>
-      <Card item={item2}/>
-      
+      {items.map(function(element){
+        return <Card item={element} />
+      })}      
     </>
   )
 }
