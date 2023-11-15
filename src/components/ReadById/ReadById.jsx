29e96c6d072afbc46964badbb726113c57c7fed6
luizantoniofc <150301604+luizantoniofc@users.noreply.github.com>
2023-11-15 01:29:25 +0000

    Id adicionado

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,3 +1,8 @@
+import { useParams } from "react-router-dom"
+
 export default function ReadById() {
-    return <div>ReadById</div>
+    const { id }= useParams()
+
+    const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
+    return <div>ReadById: {id}</div>
 }
