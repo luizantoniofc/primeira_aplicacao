3809ece523b49836811df99dc62827d908e11cbb
luizantoniofc <150301604+luizantoniofc@users.noreply.github.com>
2023-11-15 01:00:43 +0000

    Inicio das Rotas

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Card from '../Card/Card';
 
 export default function Home() {
